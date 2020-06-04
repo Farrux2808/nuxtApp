c548f5f05958d0c84e97de889be664e2df96e665
ruslanraupoff <ruslanraupoff@gmail.com>
2020-06-05 00:40:24 +0500

    data-tables to data-tables-server

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,162 @@
 <template>
-<div>
-  <v-card flat class="py-12">
-    <v-card-text>
-      <v-row align="center" justify="center">
-        <v-btn-toggle v-model="toggle_exclusive"  mandatory>
-          <v-btn>
-            today
-          </v-btn>
-          <v-btn>
-            yesterday
-          </v-btn>
-          <v-btn>
-            week
-          </v-btn>
-          <v-btn>
-            year
-          </v-btn>
-        </v-btn-toggle>
-      </v-row>
-    </v-card-text>
-  </v-card>
-  <data-tables :data="data" :table-props="tableProps">
-    <el-table-column v-for="title in titles" :prop="title.prop" :label="title.label" :key="title.label" sortable="custom">
-    </el-table-column>
-  </data-tables>
-</div>
+  <div>
+    <data-tables-server v-loading="loading" :data="data" :current-page="query.currentPage" 
+      :total="query.total" @query-change="loadData" :page-size="query.limit"
+      :pagination-props="query.pageSizes"
+      :checkbox-filter-def="checkboxFilterDef">
+      <el-table-column v-for="title in titles" :prop="title.prop" :label="title.label" :key="title.prop" sortable="custom">
+      </el-table-column>
+    </data-tables-server>
+  </div>
 </template>
 
 <script>
-export default {
-  
-  data() {
-    var data, titles
+  var data = [
+    {
+      "content": "Water flood",
+      "flow_no": "FW201601010001",
+      "flow_type": "Repair",
+      "flow_type_code": "repair",
+    }, {
+      "content": "Lock broken",
+      "flow_no": "FW201601010002",
+      "flow_type": "Repair",
+      "flow_type_code": "repair",
+    }, {
+      "content": "Help to buy some drinks",
+      "flow_no": "FW201601010003",
+      "flow_type": "Help",
+      "flow_type_code": "help"
+    }
+  ]
 
-data = [{
-    "content": "Water flood",
-    "flow_no": "FW201601010001",
-    "flow_type": "Repair",
-    "flow_type_code": "repair",
-    }, {
-    "content": "Lock broken",
-    "flow_no": "FW201601010002",
-    "flow_type": "Repair",
-    "flow_type_code": "repair",
-    }, {
-    "content": "Help to buy some drinks",
-    "flow_no": "FW201601010003",
-    "flow_type": "Help",
-    "flow_type_code": "help"
-  }];
+  // fake server
+  let serverData = []
+  for (let i = 0; i < 1000; i++) {
+    serverData.push({
+      'content': 'Lock broken' + i,
+      'flow_no': 'FW20160101000' + i,
+      'flow_type': i % 2 === 0 ? 'Repair' : 'Help',
+      'flow_type_code': i % 2 === 0 ? 'repair' : 'help',
+    })
+  }
 
-  titles = [{
-    prop: "flow_no",
-    label: "NO."
-    }, {
-    prop: "content",
-    label: "Content"
-    }, {
-    prop: "flow_type",
-    label: "Type"
-  }]
-    return {
-      toggle_exclusive: undefined,
-      data,
-      titles,
-      tableProps: {
-        border: true,
-        stripe: true,
-        defaultSort: {
-          prop: 'flow_no',
-          order: 'descending'
+  let mockServer = function(res) {
+    let datas = serverData.slice()
+    let allKeys = Object.keys(data[0])
+    console.log(res)
+    // do filter
+    res.filters.forEach(filter => {
+      datas = datas.filter(data => {
+        let props = filter.props || allKeys
+        return props.some(prop => {
+          if (!filter.vals || filter.vals.length === 0) {
+            return true
+          }
+          return filter.vals.some(val => {
+            return data[prop].toString().toLowerCase().indexOf(val.toLowerCase()) > -1
+          })
+        })
+      })
+    })
+
+    // do sort
+    if (res.sort.order) {
+      let order = res.sort.order
+      let prop = res.sort.prop
+      let isDescending = order === 'descending'
+
+      datas.sort(function(a, b) {
+        if (a[prop] > b[prop]) {
+          return 1
+        } else if (a[prop] < b[prop]) {
+          return -1
+        } else {
+          return 0
         }
+      })
+      if (isDescending) {
+        datas.reverse()
       }
+    }
+
+    return {
+      data: datas.slice((res.page - 1) * res.pageSize, res.page * res.pageSize),
+      req: res,
+      ts: new Date(),
+      total: datas.length
+    }
+  }
+
+  let i = 1
+
+  // fake http
+  function http(res, time = 200) {
+    return new Promise((resolve, reject) => {
+      setTimeout(_ => {
+        i++ % 5 !== 0
+          ? resolve(mockServer(res))
+          : reject('network error')
+      }, time)
+    })
+  }
+export default {
+  data() {
+    return {
+      data: [],
+      loading: true,
+      titles: [
+        {
+          prop: "flow_no",
+          label: "NO."
+        }, {
+          prop: "content",
+          label: "Content"
+        }, {
+          prop: "flow_type",
+          label: "Type"
+        }
+      ],
+      total: 0,
+      checkboxFilterDef: {
+        props: 'flow_type_code',
+        def: [
+          {
+            'code': 'repair',
+            'name': 'Repair'
+          }, {
+            'code': 'help',
+            'name': 'Help'
+          }
+        ]
+      },
+      query: {
+          total: 100,
+          limit: 10,
+          offset: 0,
+          search: "",
+          sort: {
+              prop: "createdAt",
+              order: "ascending"
+          },
+          currentPage: 1,
+          pageSizes: { pageSizes: [10, 20, 50, 100] }
+      }
+    }
+  },
+  methods: {
+    async loadData(queryInfo) {
+      this.loading = true
+      return await http(queryInfo, 1000)
+        .then(data => {
+          this.data = data.data
+          this.total = data.total
+          this.loading = false
+        })
+        .catch(error => {
+          this.$message(error)
+          this.loading = false
+        })
     }
   }
 }
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
-import { DataTables } from 'vue-data-tables/dist/data-tables.server'
+import { DataTablesServer } from 'vue-data-tables/dist/data-tables.server'
 
+Vue.use(DataTablesServer)
 Vue.use(Element, { locale })
-Vue.use(DataTables)
